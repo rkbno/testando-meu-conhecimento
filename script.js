f0138a3dc913a7d7d0b4b2ba34978c7538384c37
Breno Teixeira <brenotm10@gmail.com>
2023-08-02 14:00:25 -0300

    aprimorando

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-function carregar () {
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
+localStorage.promptName = prompt('Qual seu nome?')
+const msg = document.getElementById('msg')
+const img = document.getElementById('imagem')
 
+function carregar () {
+    const data = new Date()
+    const hora = data.getHours()
+    const promptName = localStorage.promptName
     if (hora >= 0 && hora < 12) {
         img.src = '/src/imagens/manhã.jfif'
         document.body.style.background = '#e2cd9f'
-        msg.innerHTML = 'Bom dia'
-    } if (hora >= 12 && hora < 18) {
+        msg.innerHTML = 'Bom dia ' + localStorage.promptName
+    } 
+    else if(hora >= 12 && hora < 18) {
         img.src = '/src/imagens/tarde.jfif'
         document.body.style.background = '#b9846e'
-        msg.innerHTML = 'Boa tarde'
-    } else {
+        msg.innerHTML =  'Boa tarde ' + localStorage.promptName
+    }
+     else {
         img.src = '/src/imagens/noite.jfif'
         document.body.style.background = '#515151'
-        msg.innerHTML = 'Boa Noite'
+        msg.innerHTML = 'Boa Noite ' + localStorage.promptName
     }
 
 }
 
-function enviar () {
-    var n = document.querySelector('input#txtn')
-    var msg = document.querySelector('div#msg')
 
-    if (n == 0 ) {
-        alert('Coloque primeiro o seu nome!')
-    }
-}
 
+carregar();
